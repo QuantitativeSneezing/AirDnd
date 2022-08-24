e84ae89a2085d1eb7205f22923e34997166a834e
Jason <jatsburgern@gmail.com>
2022-08-24 18:16:28 -0400

    phase 0:

---
+++ authenticate-me/backend/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+const app = express();
+app.use(morgan("dev"))
+app.use(cookieParser());
+app.use(express.json());
+if (isProduction===false){
+    app.use(cors());
+}
+app.use(
+    helmet.crossOriginResourcePolicy({
+      policy: "cross-origin"
+    })
+  );
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
+
+// router time
+const routes = require('./routes');
+
+// ...
+
+app.use(routes); // Connect all the routes
+module.exports = app; //put this at bottom
