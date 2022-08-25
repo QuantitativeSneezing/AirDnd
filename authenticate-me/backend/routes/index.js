be39c89c54c28a5a764e1fce405e42cafb6ae246
Jason <jatsburgern@gmail.com>
2022-08-25 16:19:37 -0400

    added error handling

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const apiRouter= require("./api")
+const { ValidationError } = require('sequelize');
+const { environment } = require('../config');
+const isProduction = environment === 'production';
 router.use('/api', apiRouter)
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
@@ -10,4 +13,32 @@ router.get("/api/csrf/restore", (req, res) => {
     });
   });
 
+  //
+  router.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+  });
+  //404 handler
+  router.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+//error formatter, goes last
+  router.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
   module.exports = router;
