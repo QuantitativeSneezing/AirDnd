b9fea1c7b42fceba82cfe076ff1867845e9687dc
Jason <jatsburgern@gmail.com>
2022-08-28 06:00:15 -0400

    added auth middleWare

--- authenticate-me/backend/db/utils/auth.js
+++ authenticate-me/backend/db/utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
-const { jwtConfig } = require('../config');
-const { User } = require('../db/models');
+const { jwtConfig } = require('../../config');
+const { User } = require('../models');
 
 const { secret, expiresIn } = jwtConfig;
 const setTokenCookie = (res, user) => {
--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,7 +1,31 @@
 const router = require('express').Router();
+const { setTokenCookie, restoreUser, requireAuth } = require("../../db/utils/auth");
+const { User } = require("../../db/models")
+router.post('/test', function (req, res) {
+  res.json({ requestBody: req.body });
+});
+router.use(restoreUser);
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
+router.get('/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+router.get('/set-token-cookie', async (_req, res) => {
+  console.log("hello")
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
   });
-
+  setTokenCookie(res, user);
+  return res.json({ user });
+});
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 module.exports = router;
