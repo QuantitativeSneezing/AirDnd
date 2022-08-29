da73bcc4e7d003152ef2a4c36005038a0bf1c429
Jason <jatsburgern@gmail.com>
2022-08-28 21:16:31 -0400

    added login endpoint

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,16 +1,25 @@
 const router = require('express').Router();
 const { setTokenCookie, restoreUser, requireAuth } = require("../../db/utils/auth");
 const { User } = require("../../db/models")
+const usersRouter= require("./users")
+const sessionRouter= require("./session")
+
+router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.get('/restore-user',
+(req, res) => {
+  return res.json(req.user);
+}
+);
+
 router.post('/test', function (req, res) {
   res.json({ requestBody: req.body });
 });
-router.use(restoreUser);
 
-router.get('/restore-user',
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
 router.get('/set-token-cookie', async (_req, res) => {
   console.log("hello")
   const user = await User.findOne({
@@ -21,6 +30,7 @@ router.get('/set-token-cookie', async (_req, res) => {
   setTokenCookie(res, user);
   return res.json({ user });
 });
+
 router.get(
   '/require-auth',
   requireAuth,
