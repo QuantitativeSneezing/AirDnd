68f2892b7d1ec9190c354b3202bf686191288f64
Jason <jatsburgern@gmail.com>
2022-09-01 17:35:21 -0400

    added first validator to spots, forgot I was still on main

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,6 +2,43 @@ const express = require('express')
 const router = express.Router();
 const { Spot, User, SpotImage, Review, Booking } = require('../../db/models');
 const { requireAuth } = require('../../db/utils/auth')
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../db/utils/validation');
+const validateSpot = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .withMessage('Street address is required'),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage('City is required.'),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage('State is required'),
+    check('country')
+        .exists({ checkFalsy: true })
+        .withMessage('Country is required'),
+    check('lat')
+        .exists({ checkFalsy: true })
+        .isFloat({ min: -90, max: 90 })
+        .withMessage('Latitude is not valid'),
+    check('lng')
+        .exists({ checkFalsy: true })
+        .isFloat({ min: -180, max: 180 })
+        .withMessage('Longitude is not valid'),
+    check('name')
+        .exists({ checkFalsy: true })
+        .isLength({ max: 50 })
+        .withMessage('Name must be less than 50 characters'),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage('Description is required'),
+    check('price')
+        .exists({ checkFalsy: true })
+        .withMessage('Price per day is required'),
+    handleValidationErrors
+];
+
+
 
 router.get('/:id/reviews', async (req, res, next) => {
     const lookForId = req.params.id
@@ -45,8 +82,8 @@ router.post('/:id/images', async (req, res, next) => {
         where: { id: lookForId }
     });
     if (spot) {
-        const {url, preview}= req.body
-        const newSpotImage= await SpotImage.create({spotId:lookForId, url, preview})
+        const { url, preview } = req.body
+        const newSpotImage = await SpotImage.create({ spotId: lookForId, url, preview })
         res.json(newSpotImage)
     } else {
         res.status(404)
@@ -103,10 +140,12 @@ router.get('/',
 )
 
 router.post('/',
+    requireAuth,
+    validateSpot,
     async (req, res, next) => {
-        const {address, city, state, country, lat, lng, name, description, price } = req.body
+        const { address, city, state, country, lat, lng, name, description, price } = req.body
         const { user } = req;
-        const ownerId= user.id;
+        const ownerId = user.id;
         const newSpot = await Spot.create({ ownerId, address, city, state, country, lat, lng, name, description, price });
         newSpot.save();
         res.json(newSpot)
